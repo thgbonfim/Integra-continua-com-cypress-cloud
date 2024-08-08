089c610e34f02d1db75a75c9a7bf3d7055693e95
thiago thg <thiagobonfim73@gmail.com>
2024-08-08 13:18:05 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -6,9 +6,11 @@ module.exports = defineConfig({
     baseUrl: 'https://notes-serverless-app.com',
     env: {
       viewportWidthBreakpoint: 768,
-      
+    },
+    setupNodeEvents(on, config) {
+      require('@cypress/grep/src/plugin')(config)
+      return config
     },
   },
   projectId: 'ywd6nd',
-  projectId: "2dg7xr"
 })
